0f5205830908aa1be52aee2e1ddcaeb98a501122
Kulshresth Jangid <kulshresthjangid@gmail.com>
2024-10-10 23:52:42 +0530

    first commit

---
+++ lambda-expression-method-reference/Task.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Task {
+    void execute();
+}
---
+++ lambda-expression-method-reference/TaskUtility.java
@@ -0,0 +1,13 @@
+public class TaskUtility {
+    public static void main(String[] args) {
+        System.out.println("Starting the task...");
+    }
+
+    public static void stopTask() {
+        System.out.println("Stopping the task....");
+    }
+
+    public static void resetTask() {
+        System.out.println("Resetting the task.....");
+    }
+}
